9c3116a498ac96b105b50a81ad9f2354df5c32e8
ketan karlekar <karlekarketan@gmail.com>
2023-01-21 18:40:28 +0530

    add audio

--- js/app.js
+++ js/app.js
@@ -1,24 +1,28 @@
-const endDate = "21 Jan 2023 01:26:00 PM"
+
+
+const endDate = "21 Jan 2023 06:38:00 PM";
 document.getElementById("end-date").innerText = endDate;
-const inputs = document.querySelectorAll("input")
+const inputs = document.querySelectorAll("input");
 let interValid;
 
-
 const clock = () => {
-    const end = new Date(endDate)
-    const now = new Date()
-    const diff = (end - now) / 1000;
-   if (diff <= 0) {
-     clearInterval(intervalId);
-     alert("Countdown is over!");
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff % 60);
+
+   if (diff < 0) {
+     clearInterval(interValid);
+     document.getElementById("time").innerHTML = "!! HAPPY BIRTHDAY !!";
+      // alert("DONE")
+     var audio = document.getElementById("musics");
+     audio.autoplay = true;
+     audio.play();
    }
+};
 
-
-     console.log(diff);
-    inputs[0].value = (Math.floor(diff / 3600 / 24));
-    inputs[1].value = (Math.floor(diff / 3600) % 24);
-    inputs[2].value = (Math.floor(diff / 60) % 60);
-    inputs[3].value = Math.floor(diff % 60);
-    setInterval(clock, 1000);
-}
-clock()
+interValid = setInterval(clock, 1000);
